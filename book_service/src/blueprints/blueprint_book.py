c3bde7bd5491676a33a6701cbdd36449aa391b02
asanyal122 <asanyal122@gmail.com>
2022-11-27 09:44:58 +0530

    filtering logic added

--- book_service/src/blueprints/blueprint_book.py
+++ book_service/src/blueprints/blueprint_book.py
@@ -1,5 +1,6 @@
 from flask import Blueprint,request,jsonify
 from ..db import books_collection
+from bson.json_util import loads,dumps
 
 blueprint_book = Blueprint(name='blueprint_book',import_name=__name__)
 
@@ -7,3 +8,25 @@ blueprint_book = Blueprint(name='blueprint_book',import_name=__name__)
 def add_book():
     books_collection.insert_one(request.get_json())
     return jsonify({'msg':'successfull'})
+
+
+@blueprint_book.route('/filter',methods=['GET','POST'])
+def filter_book():
+    if request.method == 'GET':
+        books = list(books_collection.find())
+        for book in books:
+            book.pop('_id')
+        return loads(dumps(books))
+    elif request.method == 'POST':
+        books=[]
+        filter = {}
+        print(dict(request.form))
+        for key in request.form:
+            filter[key]={'$regex':request.form[key]}
+        print(filter)
+        books = list(books_collection.find(filter))
+        for book in books:
+            book.pop('_id')
+        return loads(dumps(books))
+
+
