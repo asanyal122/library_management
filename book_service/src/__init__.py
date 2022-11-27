a3ddd97c910ac46c9dd408564ded5e9a2e603195
asanyal122 <asanyal122@gmail.com>
2022-11-27 12:51:26 +0530

    changed name of app to __init__

---
+++ book_service/src/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+from src.blueprints.blueprint_book import blueprint_book
+
+
+def create_app():
+    app = Flask(__name__)
+    app.register_blueprint(blueprint_book,url_prefix='/api/v1/books')
+    return app
+
--- book_service/wsgi.py
+++ book_service/wsgi.py
@@ -1,5 +1,6 @@
 from flask import Flask
-from src.app import app
+from src import create_app
 
 if __name__=="__main__":
+    app = create_app()
     app.run(host='0.0.0.0',port=5000)
